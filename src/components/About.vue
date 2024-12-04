65a2e6f1ba2160962da0d3ea47dcc378daee2dac
Maksflus <ackomaks10@gmail.com>
2024-12-04 20:23:34 +0200

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Про Теслу</h1>
+    <p>Геній, мільярдер, філантроп, плейбой</p>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+  margin-left: 20px;
+}
+
+.horizontal-line {
+  width: 75%;
+  height: 1px;
+  border: 1px solid black
+}
+p{
+  font-size: 25px;
+}
+</style>
